8fd66273bc484aba7730c8d6066d4ac712544879
2 1 <korrektorrussia@mail.ru>
2022-09-20 10:03:29 +0700

    решил 1 задачу

---
+++ zad1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("ВВедите 1 числo");
+int a = int.Parse(Console.ReadLine()) ;
+Console.WriteLine("Введите 2ое число");
+int b = int.Parse(Console.ReadLine()) ;
+int max = 0;
+int min = 0;
+if(a>b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine($"max = {max}");
+Console.Write($"min = {min}  ");
