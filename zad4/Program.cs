41cd3ca8673502c466b14f27382b997e9c0e5d63
2 1 <korrektorrussia@mail.ru>
2022-09-21 12:01:29 +0700

    zad4 done

---
+++ zad4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine()) ;
+int chetnie = 2;
+while (chetnie<=N)
+{
+    Console.Write($"{chetnie}, ");
+    chetnie+=2;
+}
+
+
+
