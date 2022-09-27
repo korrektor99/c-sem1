3b195eb37e77fb7f2b91594b6c9d875436715014
2 1 <korrektorrussia@mail.ru>
2022-09-27 11:02:41 +0700

    sem3zad2 done

---
+++ sem3dz2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.WriteLine("Введите координату х1");
+int x1 = int.Parse(Console.ReadLine()) ;
+Console.WriteLine("Введите координату y1");
+int y1 = int.Parse(Console.ReadLine()) ;
+Console.WriteLine("Введите координату z1");
+int z1 = int.Parse(Console.ReadLine()) ;
+Console.WriteLine("Введите координату х2");
+int x2 = int.Parse(Console.ReadLine()) ;
+Console.WriteLine("Введите координату y2");
+int y2 = int.Parse(Console.ReadLine()) ;
+Console.WriteLine("Введите координату z2");
+int z2 = int.Parse(Console.ReadLine()) ;
+double range= Math.Sqrt((Math.Pow((x1-x2),2) +Math.Pow((y1-y2),2) +Math.Pow((z1-z2),2)));
+Console.WriteLine($"{range}");
+
