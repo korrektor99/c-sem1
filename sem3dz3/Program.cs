9ed4820ec5d6c068146770babce5366a16852192
2 1 <korrektorrussia@mail.ru>
2022-09-27 11:14:46 +0700

    sem3zad3 done

---
+++ sem3dz3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число N");
+double N = int.Parse(Console.ReadLine()) ;  
+while (1<=(N))
+{
+    double tablitsa=Math.Pow((N),3);
+    Console.Write($"{tablitsa}, ");
+    N+=-1;
+}
+
