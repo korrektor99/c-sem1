28bbba0422326ae325ac08587e8305fe866681f0
2 1 <korrektorrussia@mail.ru>
2022-09-27 10:29:24 +0700

    sem3 dz1 done

---
+++ sem3dz1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите 5-х значное число");
+int x = int.Parse(Console.ReadLine()) ;
+if(x/10000==x%10)
+{
+    if(x/1000==x%100)
+    {
+        Console.WriteLine($" число {x}-палиндром");
+    }
+    else
+    {
+        Console.WriteLine($" число {x}- не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($" число {x}- не палиндром");
+}
