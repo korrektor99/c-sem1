06fd8bab8eff0b2d25605b39db4f58e2c091e185
2 1 <korrektorrussia@mail.ru>
2022-09-27 14:05:05 +0700

    sem4dz1 done

---
+++ sem4dz1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Введите число A");
+double A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B");
+double B = int.Parse(Console.ReadLine());
+double C = Math.Pow(A,B);  
+Console.WriteLine($"{C}");
