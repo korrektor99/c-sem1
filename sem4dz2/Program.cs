9ba77e9cbe2eb58e81eac14da6e2f45756ab983c
2 1 <korrektorrussia@mail.ru>
2022-09-27 14:05:26 +0700

    sem4dz2 done

---
+++ sem4dz2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine()) ;  
+int Sum = 0;
+while(N!=0)
+{
+    
+    
+    Sum=N%10+Sum;
+    N=N/10;
+}
+Console.WriteLine($"{Sum}");
