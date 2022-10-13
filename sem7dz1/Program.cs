306110f16e83061a5baa569981f87201cb261539
2 1 <korrektorrussia@mail.ru>
2022-10-13 12:13:36 +0700

    dz7.1 done

---
+++ sem7dz1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Console.WriteLine("Введите m");
+int m= int.Parse(Console.ReadLine());
+Console.WriteLine("Введите n");
+int n= int.Parse(Console.ReadLine());
+double[,] array= new double[m, n];
+Random rand = new Random();
+for(int y=0;y<m;y++)
+{
+    for(int j=0;j<n;j++)
+    {
+        array[y,j]=rand.NextDouble()*rand.Next(-1000,1000);
+        Console.Write($"|{array[y,j]}|    ");
+    }
+    Console.WriteLine();
+}
