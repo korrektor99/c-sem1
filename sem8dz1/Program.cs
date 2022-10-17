e0200fd9865717192e283821e39c184c57854d8c
2 1 <korrektorrussia@mail.ru>
2022-10-17 23:23:04 +0700

    dz8.1 done

---
+++ sem8dz1/Program.cs
@@ -0,0 +1,41 @@
+﻿//  Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+int[,] array = new int[4, 4];
+Random rand = new Random();
+int container = 0;
+Console.WriteLine("исходный массив");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = rand.Next(-10, 10);
+        Console.Write($"|{array[i, j]}|    ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("сортировка по строкам массива");
+int count=0;
+while (count<array.GetLength(0))
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+
+    for (int j = 0; j < array.GetLength(1) - 1; j++)
+    {
+        if (array[i, j+1] < array[i, j])
+        {
+            container = array[i, j + 1];
+            array[i, j+ 1] = array[i, j];
+            array[i, j] = container;
+        }
+    }
+}
+count++;
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write($"|{array[i, j]}|    ");
+    }
+    Console.WriteLine();
+}
